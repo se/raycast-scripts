74e1ca9fc9294358715bc44e9dd3ee19208086d4
Selcuk Ermaya <selcukermaya@gmail.com>
2024-03-05 13:48:35 +0300

    Add new service.

---
+++ docker-service-logs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Bu script, belirtilen Docker Swarm servisinin loglarını getirir ve ctrl+c ile düzgün bir şekilde durdurulabilir.
+
+# Kullanım: ./dsl <servis-ismi>
+
+# Gerekli değişkenler
+SERVICE_NAME=$1
+DOCKER_MASTER="$DOCKER_MASTER_USER@$DOCKER_MASTER_HOST"
+SSH_OPTS="-o ServerAliveInterval=60"
+
+# ctrl+c basıldığında çalışacak fonksiyon
+function cleanup {
+	echo "Stopping..."
+	exit 0
+}
+
+# Catch Ctrl+C
+trap cleanup SIGINT
+
+# Find Service Node
+NODE=$(ssh $SSH_OPTS $DOCKER_MASTER "docker service ps $SERVICE_NAME --filter 'desired-state=running' --format '{{.Node}}' | head -n 1")
+
+if [ -z "$NODE" ]; then
+	echo "Service node not found"
+	exit 1
+fi
+
+CONTAINER_ID=$(ssh $SSH_OPTS root@$NODE "docker ps --filter 'name=$SERVICE_NAME' --format '{{.ID}}' | head -n 1")
+
+if [ -z "$CONTAINER_ID" ]; then
+	echo "Service not found"
+	exit 1
+fi
+
+# Get Logs
+ssh -t $SSH_OPTS root@$NODE "docker logs $CONTAINER_ID --tail 200 -ft"
